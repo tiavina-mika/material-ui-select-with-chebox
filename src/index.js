811342ff365ba71429692c9030e9a2317a442802
TiavinaMichael <ralainirinatiavina@gmail.com>
2022-03-21 18:25:07 +0300

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SelectWithCheckbox from "./SelectWithCheckbox";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Demo from './demo';
-
-ReactDOM.render(<Demo />, document.querySelector('#root'));
-    
+ReactDOM.render(<SelectWithCheckbox />, document.querySelector("#root"));
